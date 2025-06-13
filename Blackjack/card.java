d941111f9f322f07a9927febd495f6bf74680dab
SantosFarias10 <santos.farias@mi.unc.edu.ar>
2025-06-13 11:29:45 -0300

    {M} implementacion de metodos de card

---
+++ Blackjack/card.java
@@ -0,0 +1,46 @@
+public class card {
+    private String suit;    // Palo: Corazon, Diamante, Trebol, Pica.
+    private String rank;    // 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A.
+    private int value;      // J=10, Q=10, K=10, A=11.
+
+    public card(String suit, String rank, int value){
+        this.suit = suit;
+        this.rank = rank;
+        this.value = value;
+    }
+
+    // Metodo para verificar si son del mismo numero
+    public boolean equalsRank(card c1, card c2) {
+        return c1.getRank().equals(c2.getRank());
+    }
+
+    // Getters y setters
+    public String getSuit() {
+        return suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+    
+    // metodo para imprimir la cards
+    public String toString() {
+        return rank + " de " + suit;
+    }
+}
