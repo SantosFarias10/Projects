5fc1624f53810be8d4261ecdbacb0688c2609fc4
SantosFarias10 <santos.farias@mi.unc.edu.ar>
2025-12-09 21:18:54 +0000

    {M} main.go: Simulacion de un nuevo departamento con cambio de precio

--- deptoHunter/backend/main.go
+++ deptoHunter/backend/main.go
@@ -6,17 +6,50 @@ import (
 	// define codigos de estado http (200 ok, 404 not fount, ...), maneja las requests y responses, permite crear servidores web basicos
 	"net/http"
 
+	"time"
+
 	// framework web
 	// facilita la creciacion de APIs REST, maneja endpoints, procesa JSON, incluye middleware (autenticacion, logging, ...)
 	"github.com/gin-gonic/gin"
 
 	// Paquete que contiene la conexion a la base de datos
 	"github.com/SantosFarias10/deptoHunter/config"
+
+	"github.com/SantosFarias10/deptoHunter/service"
 )
 
 func main() {
+	// Seteamos el modo de desarrollo, ahora no mostrara logs
+	gin.SetMode(gin.ReleaseMode)
+
 	// Conectamos a la base de datos
 	config.ConnectDB()
+
+	// Simulacion que el scraper encontro un depto hoy
+	service.ProcesarDepto(
+		"Depto 1",
+		"http://web-inmobiliaria.com/depto-123",
+		"Centro, Cordoba",
+		3,
+		1,
+		25.3,
+		50000.00,
+		"USD",
+	)
+
+	// Simulacion que pasaron unos segundos y el scraper volvio a pasar
+	// pero el precio bajo a 45000.00
+	time.Sleep(2 * time.Second) // 2 segundos
+	service.ProcesarDepto(
+		"Depto 1",
+		"http://web-inmobiliaria.com/depto-123",
+		"Centro, Cordoba",
+		3,
+		1,
+		25.3,
+		45000.00,
+		"USD",
+	)
 
 	// Inicializamos el server con Logger + Recovery
 	// Basicamente inicializamos el router con middleware, si ocurre un error lo captura y evita que el server se caiga
@@ -38,7 +71,7 @@ func main() {
 
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, Response{
-			Mensaje: "Base de Datos conectada!",
+			Mensaje: "Sistema Corriendo!",
 		})
 	})
 
