c902443a7886bc83ffb58f40a6f1e86959200c7a
SantosFarias10 <santos.farias@mi.unc.edu.ar>
2025-12-09 21:20:18 +0000

    {M} db.go: config para que no muestre los loggers en la terminal

--- deptoHunter/backend/config/db.go
+++ deptoHunter/backend/config/db.go
@@ -12,6 +12,8 @@ import (
 	"gorm.io/driver/sqlite"
 
 	"gorm.io/gorm"
+
+	"gorm.io/gorm/logger"
 )
 
 // Variable global que almacena la conexion a la base de datos
@@ -20,8 +22,12 @@ var DB *gorm.DB
 func ConnectDB() {
 	var err error
 
+	configuration := &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	}
+
 	// conectamos a la base de datos, gorm.Open() abre una conexion a la base de datos y retorna si todo sale bien (conexion, nil), en caso contrario (nil, error)
-	DB, err = gorm.Open(sqlite.Open("base_de_datos.db"), &gorm.Config{})
+	DB, err = gorm.Open(sqlite.Open("base_de_datos.db"), configuration)
 	if err != nil {
 		log.Fatal("Error al conectar a la base de datos: ", err)
 	}
