bef66af2665812fdcccc535627aec4ac15399778
SantosFarias10 <santos.farias@mi.unc.edu.ar>
2025-12-10 00:12:57 +0000

    {ADD} main.go: scraping en argenprop

--- deptoHunter/backend/main.go
+++ deptoHunter/backend/main.go
@@ -6,8 +6,6 @@ import (
 	// define codigos de estado http (200 ok, 404 not fount, ...), maneja las requests y responses, permite crear servidores web basicos
 	"net/http"
 
-	"time"
-
 	// framework web
 	// facilita la creciacion de APIs REST, maneja endpoints, procesa JSON, incluye middleware (autenticacion, logging, ...)
 	"github.com/gin-gonic/gin"
@@ -15,7 +13,7 @@ import (
 	// Paquete que contiene la conexion a la base de datos
 	"github.com/SantosFarias10/deptoHunter/config"
 
-	"github.com/SantosFarias10/deptoHunter/service"
+	"github.com/SantosFarias10/deptoHunter/scraper"
 )
 
 func main() {
@@ -25,31 +23,8 @@ func main() {
 	// Conectamos a la base de datos
 	config.ConnectDB()
 
-	// Simulacion que el scraper encontro un depto hoy
-	service.ProcesarDepto(
-		"Depto 1",
-		"http://web-inmobiliaria.com/depto-123",
-		"Centro, Cordoba",
-		3,
-		1,
-		25.3,
-		50000.00,
-		"USD",
-	)
-
-	// Simulacion que pasaron unos segundos y el scraper volvio a pasar
-	// pero el precio bajo a 45000.00
-	time.Sleep(2 * time.Second) // 2 segundos
-	service.ProcesarDepto(
-		"Depto 1",
-		"http://web-inmobiliaria.com/depto-123",
-		"Centro, Cordoba",
-		3,
-		1,
-		25.3,
-		45000.00,
-		"USD",
-	)
+	urlArgenProp := "https://www.argenprop.com/departamentos/alquiler/cordoba-arg"
+	go scraper.ScrapearSitio(urlArgenProp)
 
 	// Inicializamos el server con Logger + Recovery
 	// Basicamente inicializamos el router con middleware, si ocurre un error lo captura y evita que el server se caiga
